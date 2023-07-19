0911b94d4bd9bc3346e05669202398fe9317be6c
Mohamed Mihammi <momihamm@e3r5p8.1337.ma>
2023-07-19 08:41:31 +0100

    pref : leaks

--- push_swap.c
+++ push_swap.c
@@ -6,20 +6,27 @@
 /*   By: momihamm <momihamm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 19:42:06 by momihamm          #+#    #+#             */
-/*   Updated: 2023/07/16 11:03:28 by momihamm         ###   ########.fr       */
+/*   Updated: 2023/07/19 08:37:59 by momihamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void    mr (void)
+{
+    system ("leaks push_swap");
+}
 
 int main (int ac, char **av)
 {
     if (ac >= 2)
     {
         t_parss *pars;
+        char    *ptr;
 
         pars = malloc (sizeof (t_parss));
         pars->arg1 = malloc (1);
+        ptr = pars->arg1;
         pars->indxarg = 0;
         while (av[++pars->indxarg])
             pars->arg1 = ft_strjoin (pars->arg1, finale_char (av[pars->indxarg]));
@@ -32,5 +39,7 @@ int main (int ac, char **av)
         // printf ("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
         // print_nodes (pars->stack_b);
         print_num (&pars->head);
+        free (ptr);
     }
+    atexit (mr);
 }
