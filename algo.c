b8e85cb48850e8580c85f00a8852bfe9f1be6047
m0m1h4mm <m0m1h4mm@kali.m0m1h4mm>
2023-09-03 18:10:57 +0100

    chore : bild and use LIS in array of random numbers and yep it's work

--- algo.c
+++ algo.c
@@ -30,6 +30,13 @@ int main()
     i = 0;
     while (i < 12)
     {
+        printf ("|%d|\t",matrix[0][i]);
+        i++;
+    }
+    printf ("\n");
+    i = 0;
+    while (i < 12)
+    {
         printf ("<%d>\t",matrix[1][i]);
         i++;
     }
@@ -40,5 +47,32 @@ int main()
         printf (">%d<\t",matrix[2][i]);
         i++;
     }
-   
+    i = 0;
+    int m=0;
+    while (i < 12)
+    {
+        if (matrix[1][i] > m)
+            m = matrix[1][i];
+        i++;
+    }
+    printf ("\nTHE LIS : %d\n",m);
+    i = 11;
+    int n =m;
+    int *lis = calloc (m, sizeof (int));
+    while (i >= 0)
+    {
+        if (matrix[1][i] == n)
+        {
+            lis[n - 1] = matrix[0][i];
+            n--;
+        }
+        i--;
+    }
+    i = 0;
+    while (i < 6)
+    {
+        printf ("!%d!\t",lis[i]);
+        i++;
+    }
+    free (lis);
 }
